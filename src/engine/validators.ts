9e5aed12fea0ee1ed99760ee8f40d31fcc4b8c2f
Ophir Bushinsky <ophirbushi@gmail.com>
2024-03-22 13:21:01 +0200

    build validate input

--- src/engine/validators.ts
+++ src/engine/validators.ts
@@ -1,10 +1,12 @@
-import { isTemporarilyIllegalTile } from './helpers';
+import { getTileByIndex, isTemporarilyIllegalTile } from './helpers';
 import { State } from './models';
 
-export const validateInput = (state: State, input: any): boolean => {
+export const validateInput = (state: State, input: unknown): boolean => {
   switch (state.phaseId) {
     case 'build':
-      if (isTemporarilyIllegalTile(state, input)) {
+      const tileIndex = input as number
+      const tile = getTileByIndex(state, tileIndex)
+      if (isTemporarilyIllegalTile(state, tile)) {
         return false
       }
       break;
