db3c82c0eb73a25f37cd4f37eb052a2ea748954c
Ophir Bushinsky <ophirbushi@gmail.com>
2024-03-30 00:14:59 +0300

    refactor

--- src/server/index.ts
+++ src/server/index.ts
@@ -36,70 +36,76 @@ const runGame = (gameId: string, state: State): EventEmitterGameInstance => {
 
 const games: { [gameId: string]: EventEmitterGameInstance } = {}
 
-express()
-  .use(express.json())
-  .use(cors())
-  .post('/game', async (req, res) => {
-    try {
-      const gameId = Date.now().toString()
-      const config = req.body.config || defaultConfig
-      const metadata = req.body.metadata || {}
-      const gameInstance = runGame(gameId, initState(config, metadata))
-      res.send({ gameId, state: gameInstance.state })
-    } catch (err) {
-      res.status(500).send({ error: err })
-    }
-  })
-  .get('/game/:gameId/state', async (req, res) => {
-    try {
-      const gameId = req.params.gameId
-      let gameInstance = getGameInstance(gameId)
-      if (!gameInstance) {
-        const state = await fetchGameState(gameId)
-        if (!state) {
-          res.status(404).send({ error: 'game not found' })
-          return
-        }
-        gameInstance = runGame(gameId, state)
-      }
-      res.send(gameInstance.state)
-    } catch (err) {
-      res.status(500).send({ error: err })
-    }
-  })
-  .post('/game/:gameId/input', async (req, res) => {
-    const input = req.body.input
+const newGame = async (req: express.Request, res: express.Response) => {
+  try {
+    const gameId = Date.now().toString()
+    const config = req.body.config || defaultConfig
+    const metadata = req.body.metadata || {}
+    const gameInstance = runGame(gameId, initState(config, metadata))
+    res.send({ gameId, state: gameInstance.state })
+  } catch (err) {
+    res.status(500).send({ error: err })
+  }
+}
+
+const getGameState = async (req: express.Request, res: express.Response) => {
+  try {
     const gameId = req.params.gameId
-    if (!input || !gameId) {
-      res.status(400).send({ error: 'missing input or gameId' })
-      return
-    }
-    try {
-      const gameInstance = getGameInstance(gameId)
-      if (!gameInstance) {
+    let gameInstance = getGameInstance(gameId)
+    if (!gameInstance) {
+      const state = await fetchGameState(gameId)
+      if (!state) {
         res.status(404).send({ error: 'game not found' })
         return
       }
-      if (!gameInstance.isRunning) {
-        res.status(400).send({ error: 'game not running', gameError: gameInstance.error })
-        return
-      }
-      const state = gameInstance.state
-      const inputValid = validateInput(state, input)
-      if (!inputValid) {
-        res.status(400).send({ error: 'input invalid' })
-        return
-      }
-      gameInstance.input.postInput(input)
-      // TODO: remove:
-      await new Promise((resolve) => setTimeout(resolve, 5))
-      if (!gameInstance.isRunning) {
-        res.status(400).send({ error: 'input crashed game', gameError: gameInstance.error })
-        return
-      }
-      res.send({ state: gameInstance.state })
-    } catch (err) {
-      res.status(500).send({ error: err })
+      gameInstance = runGame(gameId, state)
     }
-  })
+    res.send(gameInstance.state)
+  } catch (err) {
+    res.status(500).send({ error: err })
+  }
+}
+
+const postGameInput = async (req: express.Request, res: express.Response) => {
+  const input = req.body.input
+  const gameId = req.params.gameId
+  if (!input || !gameId) {
+    res.status(400).send({ error: 'missing input or gameId' })
+    return
+  }
+  try {
+    const gameInstance = getGameInstance(gameId)
+    if (!gameInstance) {
+      res.status(404).send({ error: 'game not found' })
+      return
+    }
+    if (!gameInstance.isRunning) {
+      res.status(400).send({ error: 'game not running', gameError: gameInstance.error })
+      return
+    }
+    const state = gameInstance.state
+    const inputValid = validateInput(state, input)
+    if (!inputValid) {
+      res.status(400).send({ error: 'input invalid' })
+      return
+    }
+    gameInstance.input.postInput(input)
+    // TODO: remove:
+    await new Promise((resolve) => setTimeout(resolve, 5))
+    if (!gameInstance.isRunning) {
+      res.status(400).send({ error: 'input crashed game', gameError: gameInstance.error })
+      return
+    }
+    res.send({ state: gameInstance.state })
+  } catch (err) {
+    res.status(500).send({ error: err })
+  }
+}
+
+express()
+  .use(express.json())
+  .use(cors())
+  .post('/game', newGame)
+  .get('/game/:gameId/state', getGameState)
+  .post('/game/:gameId/input', postGameInput)
   .listen(3000)
