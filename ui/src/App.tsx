cc9a028f6ede7f652a033bcc9ec10a5d5f6b4aff
Ophir Bushinsky <ophirbushi@gmail.com>
2025-03-29 21:17:12 +0300

    project restructure

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Box, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <Box my={2}>
+        <Typography variant="h4">Acquire Game UI</Typography>
+        {/* Your components go here */}
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
